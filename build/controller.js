eccdcb449bfc64501ad3e56afafd61c199a7bc41
Nir Nicole <Niko426@gmail.com>
2022-09-07 17:28:19 +0300

    compsed all rendering methods into a one template method

--- build/controller.js
+++ build/controller.js
@@ -1,16 +1,13 @@
 "use strict";
 const model = rupgModel();
 const renderer = rupgRender();
-const data = model.getData().then(res => {
-    //here the res data{users: Promise, kanye: Promise} is supposed ready to be passed to the renderer
-    console.log(res);
-    renderer.renderUsers(res.users.userList);
-    renderer.renderMainUser(res.users.mainUser);
-    renderer.renderQuote(res.kanye.proccesedData);
-    renderer.renderPokemon(res.pokemon.frontPicture, res.pokemon.pname);
-    renderer.renderAboutMe(res.bacon.proccesedData);
-    return res;
-});
+const generateUser = function () {
+    const data = model.getData().then(res => {
+        //here the res data{users: Promise, kanye: Promise} is supposed ready to be passed to the renderer
+        renderer.renderPage(res);
+        return res;
+    });
+};
 // console.log(data);
 // data.then(res=> console.log(res))
 //event should hold async function ,
